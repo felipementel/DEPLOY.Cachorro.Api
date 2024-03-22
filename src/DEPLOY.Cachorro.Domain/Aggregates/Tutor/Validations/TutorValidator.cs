58faec6b14b23dc2dc06ae92a258485290bc2254
Felipe Augusto <felipe.augusto@avanade.com>
2024-03-22 18:43:45 -0300

    feat: validando cpf do tutor com FluentValidation

--- src/DEPLOY.Cachorro.Domain/Aggregates/Tutor/Validations/TutorValidator.cs
+++ src/DEPLOY.Cachorro.Domain/Aggregates/Tutor/Validations/TutorValidator.cs
@@ -15,7 +15,51 @@ namespace DEPLOY.Cachorro.Domain.Aggregates.Tutor.Validations
                 RuleFor(x => x.Nome)
                .MaximumLength(100)
                .WithMessage("Nome deve ter no máximo 100 caracteres");
+
+                RuleFor(x => x.CPF)
+                .NotEmpty()
+                .WithMessage("CPF é obrigatório");
+
+                RuleFor(x => x.CPF.ToString().PadLeft(11,'0'))
+                .Must(IsCpf)
+                .WithMessage("CPF inválido");
             });
+        }
+
+        public bool IsCpf(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cpf.EndsWith(digito);
         }
     }
 }
